ecfba956d4c2aabee3c6d3de15431dff902bca80
Matvei Tabelev <matvei.tabelev@gmail.com>
2023-11-13 16:23:03 +0100

    Ajout exo4

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-let nombre1 = parseInt(prompt("Saisir le 1 nombre"));
-let nombre2 = parseInt(prompt("Saisir le 2 nombre"));
+/*let nombre1 = parseInt(prompt("Saisir le 1 nombre"));
+let nombre2 = parseInt(prompt("Saisir le 2 nombre"));*/
 
 //afficher dans la console si le produit est négatif
- function test(nmbr1,nmbr2) {
-    if (nmbr1 > 0 && nmbr2 > 0 || nmbr1 < 0 && nmbr2 < 0 ){
+let age = parseInt(prompt("Saisir l'âge de l'enfant"));
+ function ageCategory(n) {
+    if (n >12 ){
      
-        console.log("Le nombre est positif");        
-    }else if( nmbr1 == 0 || nmbr2 == 0 ){
-        console.log("Le nombre vaut zéro");
+        console.log("La catégorie de l'enfant est Cadet"  );        
+    }else if( n >= 10 && n <= 12 ){
+        console.log("La catégorie de l'enfant est Minime");
     }
-    else {
-    
-        console.log("Le nombre est négatif");
+    else if( n >= 8 && n <= 9 ){
+        console.log("La catégorie de l'enfant est Pupille");
     }
+    else if( n >= 6 && n <= 8 ){
+        console.log("La catégorie de l'enfant est Poussin");
+    }else{
+        n<6
+        return console.log("Hors catégorie ou trop jeune")
+    }
+ 
     
  }
 
- test(nombre1,nombre2);
+ ageCategory(age);
 
 
- /*if (nmbr1 < 0 || nmbr2 < 0 , "négatif")*/
+ /*i
+            "Poussin" de 6 à 7 ans
+           -"Pupille" de 8 à 9 ans
+           -"Minime" de 10 à 11 ans
+           -"Cadet" après 12 ans
+           -"Hors catégorie ou trop jeune" si inférieur à 6 ans*/
