b5fcc556c80ba536b1a35b2d025545c46f685358
Matvei Tabelev <matvei.tabelev@gmail.com>
2023-11-13 12:05:57 +0100

    test

--- main.js
+++ main.js
@@ -1,56 +1,12 @@
-
-/*Version button submit Si  partie html <input type="submit" id="bt" value="ajouter" click= "ajouter">
-                AU LIEU DE type="button"
-
-
-document.getElementById('form').onsubmit = (ev) => {
-    //evite de rafraichir la page
-    ev.preventDefault();
-    const nameInput = document.getElementById('name').value;
-    console.log(nameInput);
-    const firstnameInput = document.getElementById('firstname').value;
-    console.log(firstnameInput);
-    const emailInput = document.getElementById('email').value;
-    console.log(emailInput);
-
-    let utili =[];
-    utili.push(nameInput);
-    utili.push(firstnameInput);
-    utili.push(emailInput);
-    console.log(utili);
-    
+let nombre = prompt("Saisir le nombre de notes");
+let notes = [] 
+for (let i = 0; i < nombre; i++) {
+   let note = parseInt(prompt("saisir une note "));
+   notes.push(note);
 }
-
-*/
-
-
-/*function ajouter() {
+let total = 0;
+function totalNote(sum) {
 
 
-    const nameInput = document.getElementById('name').value;
-    const firstnameInput = document.getElementById('firstname').value;
-    const emailInput = document.getElementById('email').value;
-
-
-    let userArray = [];
-    userArray.push(nameInput);
-    userArray.push(firstnameInput);
-    userArray.push(emailInput);
-    
-    console.log(userArray);
-  }
-ajouter();*/
-
-document.getElementById('bt').addEventListener('click', () =>{
-
-    let nom = document.getElementById('name').value;
-    let prenom = document.getElementById('firstname').value;
-    let email = document.getElementById('email').value;
-
-    let utili = [];
-
-    utili.push(nom,prenom,email)
-    console.log(utili);
- 
-
-})
+console.log(sum);    
+}
