26f5b1520f6096b8a4c0fdd263d83ff22de6ff6a
Matvei Tabelev <matvei.tabelev@gmail.com>
2023-11-13 12:28:35 +0100

    fin

--- main.js
+++ main.js
@@ -4,9 +4,30 @@ for (let i = 0; i < nombre; i++) {
    let note = parseInt(prompt("saisir une note "));
    notes.push(note);
 }
-let total = 0;
-function totalNote(sum) {
 
 
-console.log(sum);    
-}
+
+
+    function calculSum(notes) {
+        let somme = 0;
+        let nmbrDeNotes = notes.length;
+
+        // Somme des notes
+        for (let i = 0; i < nmbrDeNotes; i++) {
+            somme += notes[i];
+            
+        }
+
+        // Calcul de la moyenne
+        let moyenne = somme / nmbrDeNotes;
+
+        return moyenne;
+    }
+
+ 
+
+    // Appel de la fonction pour calculer la moyenne
+    let moyenneDesNotes = calculSum(notes);
+
+    // Affichage de la moyenne dans la console
+    console.log("Moyenne des notes : " + moyenneDesNotes);
