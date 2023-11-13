b7bb75648e69cdff523d08dfe3040e11051847fc
Matvei Tabelev <matvei.tabelev@gmail.com>
2023-11-13 11:54:43 +0100

    Ajout correction

--- main.js
+++ main.js
@@ -1,14 +1,56 @@
-const nameInput = document.getElementById('name');
-const firstnameInput = document.getElementById('firstname');
-const emailInput = document.getElementById('email');
 
-const name = nameInput.value;
-const firstname = firstnameInput.value;
-const email = emailInput.value;
+/*Version button submit Si  partie html <input type="submit" id="bt" value="ajouter" click= "ajouter">
+                AU LIEU DE type="button"
 
-const userArray = [];
-userArray.push(name);
-userArray.push(firstname);
-userArray.push(email);
 
-console.log(userArray);
+document.getElementById('form').onsubmit = (ev) => {
+    //evite de rafraichir la page
+    ev.preventDefault();
+    const nameInput = document.getElementById('name').value;
+    console.log(nameInput);
+    const firstnameInput = document.getElementById('firstname').value;
+    console.log(firstnameInput);
+    const emailInput = document.getElementById('email').value;
+    console.log(emailInput);
+
+    let utili =[];
+    utili.push(nameInput);
+    utili.push(firstnameInput);
+    utili.push(emailInput);
+    console.log(utili);
+    
+}
+
+*/
+
+
+/*function ajouter() {
+
+
+    const nameInput = document.getElementById('name').value;
+    const firstnameInput = document.getElementById('firstname').value;
+    const emailInput = document.getElementById('email').value;
+
+
+    let userArray = [];
+    userArray.push(nameInput);
+    userArray.push(firstnameInput);
+    userArray.push(emailInput);
+    
+    console.log(userArray);
+  }
+ajouter();*/
+
+document.getElementById('bt').addEventListener('click', () =>{
+
+    let nom = document.getElementById('name').value;
+    let prenom = document.getElementById('firstname').value;
+    let email = document.getElementById('email').value;
+
+    let utili = [];
+
+    utili.push(nom,prenom,email)
+    console.log(utili);
+ 
+
+})
