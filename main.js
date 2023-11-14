b907d917fdd19978a01ae673d5ff2773653494e6
Matvei Tabelev <matvei.tabelev@gmail.com>
2023-11-14 12:05:16 +0100

    ajout exo7

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-const item = [];
+// Initialisation du tableau d'utilisateurs
+let users = [];
 
-const nameitem = prompt("Entrez le nom de l'article : ");
-const priceHT = parseFloat(prompt("Entrez le prix HT de l'article : "));
-const quantity = parseInt(prompt("Entrez la quantité de l'article : "));
+// Boucle pour ajouter 5 utilisateurs
+for (let i = 0; i < 5; i++) {
+  // Saisie des informations utilisateur
+  let nom = prompt(`Saisir le nom de l'utilisateur ${i + 1}`);
+  let prenom = prompt(`Saisir le prénom de l'utilisateur ${i + 1}`);
+  let age = prompt(`Saisir l'âge de l'utilisateur ${i + 1}`);
 
-item.push(nameitem);
-item.push(priceHT);
-item.push(quantity);
+  // Création d'un objet utilisateur
+  let user = {
+    nom: nom,
+    prenom: prenom,
+    age: parseInt(age), // Conversion de l'âge en nombre
+  };
 
-// Calculez le prix TTC
-const priceTTC = priceHT * 1.2 * quantity;
+  // Ajout de l'utilisateur au tableau
+  users.push(user);
+}
 
-// Afficher le prix TTC
-console.log("Le prix TTC de l'article", nameitem, "est de", priceTTC, "euros.");
+// Recherche de l'utilisateur le plus jeune
+let utilisateurPlusJeune = users.reduce((plusJeune, utilisateur) => {
+  return utilisateur.age < plusJeune.age ? utilisateur : plusJeune;
+}, users[0]);
+
+// Affichage dans la console de l'utilisateur le plus jeune
+console.log(
+  `L'utilisateur le plus jeune est : ${utilisateurPlusJeune.prenom} ${utilisateurPlusJeune.nom}, âgé de ${utilisateurPlusJeune.age} ans.`
+);
